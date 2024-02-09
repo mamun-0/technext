fb7a6308d1a91a2fab46d566d1765b7dee3525c0
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-02-09 16:20:47 +0600

    wrap with browserrouter and set two routing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { User } from "./Components/User/User";
+import { UserDetails } from "./Components/UserDetails/UserDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <User />;
+  return (
+    <div>
+      <Route path="/" exact component={User} />
+      <Route path="/:id" exact component={UserDetails} />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
