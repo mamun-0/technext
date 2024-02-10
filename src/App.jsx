2ac9adbb74b4c1797105c65f57038cef7be794ba
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-02-10 16:09:34 +0600

    install tailwind and fix design issue, implement sort functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { User } from "./Components/User/User";
 import { UserDetails } from "./Components/UserDetails/UserDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./index.css";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="mx-8">
       <Route path="/" exact component={User} />
       <Route path="/:id" exact component={UserDetails} />
     </div>
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import "./User.css";
-
+import { sortByName, sortByEmail, sortByCompany } from "../Utils/SortFunctions";
 export function User() {
   const history = useHistory();
   const [user, setUser] = useState(null);
@@ -22,42 +21,84 @@ export function User() {
       pathname: `/${user.id}`,
     });
   }
+  function handleSelect(e) {
+    const value = e.target.value;
+    if (value === "name") {
+      setUser(sortByName(user));
+    } else if (value === "email") {
+      setUser(sortByEmail(user));
+    } else if (value === "company") {
+      setUser(sortByCompany(user));
+    }
+  }
   return user ? (
-    <div className="row row-cols-1 row-cols-md-3 g-4 p-0">
-      {user.map((user) => {
-        return (
-          <div
-            key={user.id}
-            className="col pointer"
-            onClick={() => {
-              handleNewPage(user);
-            }}
-          >
-            <div className="card h-100">
-              <div className="card-body">
-                <img src={user.image} className="card-img-top" alt="avatar" />
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">
-                    First Name : {user?.firstName}{" "}
-                  </li>
-                  <li className="list-group-item">
-                    Last Name : {user?.lastName}{" "}
-                  </li>
-                  <li className="list-group-item">Email: {user?.email} </li>
-                  <li className="list-group-item">
-                    Address : {user?.address?.address}
-                  </li>
-                  <li className="list-group-item">
-                    City : {user?.address?.city}
-                  </li>
-                </ul>
+    <div>
+      <div className="my-4">
+        <label htmlFor="sortby" className="mr-2 text-lg">
+          Sort By
+        </label>
+        <select
+          onChange={(evt) => {
+            handleSelect(evt);
+          }}
+          className="appearance-none bg-white border  px-4 py-2 pr-8 rounded shadow leading-tight inline-block cursor-pointer"
+          name="sorting"
+          id="sortby"
+        >
+          <option className="text-lg p-2">Choose</option>
+          <option className="text-lg p-2" value="name">
+            Name
+          </option>
+          <option className="text-lg p-2" value="email">
+            Email
+          </option>
+          <option className="text-lg p-2" value="company">
+            Company Name
+          </option>
+        </select>
+      </div>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {user.map((person) => {
+          return (
+            <div
+              key={person.id}
+              className="col cursor-pointer"
+              onClick={() => {
+                handleNewPage(person);
+              }}
+            >
+              <div className="card h-100">
+                <div className="card-body">
+                  <img
+                    src={person.image}
+                    className="card-img-top rounded-full"
+                    alt="avatar"
+                  />
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      First Name : {person?.firstName}{" "}
+                    </li>
+                    <li className="list-group-item">
+                      Last Name : {person?.lastName}{" "}
+                    </li>
+                    <li className="list-group-item">Email: {person?.email} </li>
+                    <li className="list-group-item">
+                      Address : {person?.address?.address}
+                    </li>
+                    <li className="list-group-item">
+                      City : {person?.address?.city}
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   ) : (
-    <h2>Loading...</h2>
+    <div className="spinner-border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
   );
 }
