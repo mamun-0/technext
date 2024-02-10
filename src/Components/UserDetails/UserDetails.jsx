de43bf7600089e79bf9c5730483492af079748c8
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-02-10 16:21:19 +0600

    place card horizontally center

--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,15 +1,19 @@
 export function UserDetails(props) {
   const { user } = props.location;
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={user?.image} className="card-img-top" alt="avatar" />
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">First Name : {user?.firstName} </li>
-        <li className="list-group-item">Last Name : {user?.lastName} </li>
-        <li className="list-group-item">Email: {user?.email} </li>
-        <li className="list-group-item">Address : {user?.address?.address}</li>
-        <li className="list-group-item">City : {user?.address?.city}</li>
-      </ul>
+    <div className="mt-4 flex justify-center">
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={user?.image} className="card-img-top" alt="avatar" />
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">First Name : {user?.firstName} </li>
+          <li className="list-group-item">Last Name : {user?.lastName} </li>
+          <li className="list-group-item">Email: {user?.email} </li>
+          <li className="list-group-item">
+            Address : {user?.address?.address}
+          </li>
+          <li className="list-group-item">City : {user?.address?.city}</li>
+        </ul>
+      </div>
     </div>
   );
 }
